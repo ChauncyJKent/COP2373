b9a5f9a3a1f8df3eab3c3e13a79936a9882c3a0b
Chauncy Kent <ChauncyJKent@gmail.com>
2025-09-13 21:53:34 -0400

    Added methods for analysis and display of expenses.

--- ChauncyKent_03_01.py
+++ ChauncyKent_03_01.py
@@ -1,9 +1,13 @@
-import functools
+from functools import reduce
 
 class ExpenseAnalyzer:
     def __init__(self):
         self.expenses = {}
-        self.amount_list = []
+        self.total_expense = 0
+        self.highest_expense = 0
+        self.highest_type = ''
+        self.lowest_expense = float('inf')
+        self.lowest_type = ''
 
     def _get_expense(self):
         invalid = True
@@ -29,17 +33,61 @@ class ExpenseAnalyzer:
                 if expense_type in self.expenses.keys():
                     raise Exception()
                 else:
-                    invalid = True
+                    invalid = False
             except:
                 print('Please enter a unique expense name.')
                 continue
 
         return expense_type
     
-    def update_records(self):
+    def _update_records(self):
         expense = self._get_expense()
         expense_type = self._get_type()
 
         self.expenses[expense_type] = expense
-        self.amount_list.append(expense)
 
+    def collect_expenses(self):
+        more = True
+        while more:
+            self._update_records()
+            answer = ''
+            invalid = True
+            while invalid:
+                answer = input('Are there more expenses? Y/N: ')
+                if answer.lower() == 'y' or answer.lower() == 'n':
+                    invalid = False
+                else:
+                    print('Please enter "Y" or "N".')
+            if answer.lower() == 'n':
+                more = False
+            else:
+                continue
+
+    def analyze_expenses(self):
+        self.total_expense = reduce(lambda x, y: x + y, self.expenses.values())
+
+        self.highest_expense = reduce(lambda x, y: x if x > y else y, self.expenses.values())
+        for key, value in self.expenses.items():
+            if value == self.highest_expense:
+                self.highest_type = key
+
+        self.lowest_expense = reduce(lambda x, y: x if x < y else y, self.expenses.values())
+        for key, value in self.expenses.items():
+            if value == self.lowest_expense:
+                self.lowest_type = key
+    
+    def display_analysis(self):
+        print()
+        print(f'The total of the expenses is: {self.total_expense}')
+        print(f'The highest expense at {self.highest_expense} was: {self.highest_type}')
+        print(f'The lowest expense at {self.lowest_expense} was: {self.lowest_type}')
+        print()
+
+def main():
+    expenses = ExpenseAnalyzer()
+    expenses.collect_expenses()
+    expenses.analyze_expenses()
+    expenses.display_analysis()
+
+if __name__ == '__main__':
+    main()
