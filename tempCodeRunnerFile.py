64f4db12bea55de786cead298bc65d96a57c595f
Chauncy Kent <ChauncyJKent@gmail.com>
2025-09-14 19:55:35 -0400

    Removed tempCodeRunnerFile from repository.

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-Creates a loop adding the entered expenses and expense types 
-        to the dictionary on each loop, then asks if there are more 
-        expenses to be entered. Includes input validation to make sure 
-        the user enters either "Y" or "N".
